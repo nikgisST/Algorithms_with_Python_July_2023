e87e34fb4cc907ca4d8c72c35c1a21e4b7338276
Nikoleta <149585913+nikgisST@users.noreply.github.com>
2024-08-23 15:33:28 +0300

    Update 1.reserve_array.py

--- 4E - Recursion/1.reserve_array.py
+++ 4E - Recursion/1.reserve_array.py
@@ -11,6 +11,15 @@ elements = input().split()
 reverse_array(0, elements)
 print(*elements)
 
+###########################################################################
+# interactive solution is better:
+elements = input().split()
+
+for left_idx in range(len(elements) // 2):
+    right_idx = len(elements) - 1 - left_idx
+    elements[left_idx], elements[right_idx] = elements[right_idx], elements[left_idx]
+print(*elements)
+
 #########################################################
 
 
