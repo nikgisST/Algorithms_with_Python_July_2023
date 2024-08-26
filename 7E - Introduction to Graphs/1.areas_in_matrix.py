283955d86621f91df00ea20e36d1c99ee5f2d524
Nikoleta <149585913+nikgisST@users.noreply.github.com>
2024-08-26 10:36:33 +0300

    Update 1.areas_in_matrix.py

--- 7E - Introduction to Graphs/1.areas_in_matrix.py
+++ 7E - Introduction to Graphs/1.areas_in_matrix.py
@@ -1,26 +1,30 @@
-def dfs(row, col, matrix, visited, parent):
+def dfs(parent, row, col, matrix, visited):
     if row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[0]):
         return
 
-    if visited[row][col]:
+    if visited[row][col]:   # if value in visited matrix is with TRUE value
         return
 
     if matrix[row][col] != parent:
         return
 
     visited[row][col] = True
-
-    dfs(row + 1, col, matrix, visited, parent)
-    dfs(row - 1, col, matrix, visited, parent)
-    dfs(row, col + 1, matrix, visited, parent)
-    dfs(row, col - 1, matrix, visited, parent)
+    dfs(parent, row + 1, col, matrix, visited)
+    dfs(parent, row - 1, col, matrix, visited)
+    dfs(parent, row, col + 1, matrix, visited)
+    dfs(parent, row, col - 1, matrix, visited)
 
 
 rows = int(input())
 cols = int(input())
 
-matrix = [list(input()) for _ in range(rows)]
-visited = [[False] * cols for _ in range(rows)]
+matrix = []    # matrix = [list(input()) for _ in range(rows)]
+visited = []   # visited = [[False] * cols for _ in range(rows)]
+
+for _ in range(rows):
+    matrix.append(list(input()))
+    visited.append([False] * cols)
+
 areas = {}
 total_areas = 0
 
@@ -30,14 +34,15 @@ for row in range(rows):
             continue
 
         key = matrix[row][col]
-
-        dfs(row, col, matrix, visited, key)
+        dfs(key, row, col, matrix, visited)
 
         if key not in areas:
             areas[key] = 0
-
         areas[key] += 1
+
         total_areas += 1
 
 print(f'Areas: {total_areas}')
-[print(f"Letter '{area}' -> {count}") for area, count in sorted(areas.items())]
+#[print(f"Letter '{area}' -> {count}") for area, count in sorted(areas.items())]
+for area, count in sorted(areas.items()):
+    print(f"Letter '{area}' -> {count}")
