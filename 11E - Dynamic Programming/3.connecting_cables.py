70b9250fcd6bf1cd901df7cd9ef888f95c287874
Nikoleta <149585913+nikgisST@users.noreply.github.com>
2024-08-27 16:01:52 +0300

    Update 3.connecting_cables.py

--- 11E - Dynamic Programming/3.connecting_cables.py
+++ 11E - Dynamic Programming/3.connecting_cables.py
@@ -1,14 +1,17 @@
-side_a = input().split()
-side_b = list(sorted(side_a))
+# side_a = input().split()         # Longest Common Subsequence
+# side_b = list(sorted(side_a))
+cables = [int(x) for x in input().split()]
 
-size = len(side_a) + 1
+size = len(cables) + 1
+positions = [pos for pos in range(1, size)]
+
 lcs = [[0] * size for _ in range(size)]
 
 for row in range(1, size):
     for col in range(1, size):
-        if side_a[row - 1] == side_b[col - 1]:
-            lcs[row][col] = lcs[row - 1][col - 1] + 1
+        if cables[row - 1] == positions[col - 1]:      # ако са еднакви последните два добавени елемента в двета събсета
+            lcs[row][col] = lcs[row - 1][col - 1] + 1   # гледаме предишния state на двата subsequence и добавяме 1
         else:
-            lcs[row][col] = max(lcs[row][col - 1], lcs[row - 1][col])
+            lcs[row][col] = max(lcs[row][col - 1], lcs[row - 1][col])  # иначе гледаме и в единия, и в другия и взимаме най-големият с общи преднидни елементи
 
 print(f'Maximum pairs connected: {lcs[-1][-1]}')
