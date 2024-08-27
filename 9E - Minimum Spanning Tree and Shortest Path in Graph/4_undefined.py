6dd51e792e406ab28afb62cee942cebf448a7f23
Nikoleta <149585913+nikgisST@users.noreply.github.com>
2024-08-27 10:42:03 +0300

    Update 4_undefined.py

--- 9E - Minimum Spanning Tree and Shortest Path in Graph/4_undefined.py
+++ 9E - Minimum Spanning Tree and Shortest Path in Graph/4_undefined.py
@@ -1,21 +1,18 @@
-from collections import deque
+from collections import deque           # shortest paths in graph: Bellman-Ford
 
 
-def reconstruct_path(parent, node):
+def find_path(parent, node):
     current_path = deque()
-
     while node is not None:
         current_path.appendleft(node)
-        node = parent[node]
-
+        node = parent[node]      # node да стане предишния node
     return current_path
 
 
 nodes = int(input())
 edges = int(input())
-
 graph = []
-
+####################### Bellman-Ford ###########################
 for _ in range(edges):
     source, destination, weight = [int(x) for x in input().split()]
     graph.append((source, destination, weight))
@@ -28,30 +25,22 @@ distance[start] = 0
 
 parent = [None] * (nodes + 1)
 
-for _ in range(nodes - 1):
-    updated = False
-
+for _ in range(nodes - 1):    # (v - 1) times
     for source, destination, weight in graph:
-        if distance[source] == float('inf'):
+        if distance[source] == float('inf'):   # няма нито един открит път, започващ от първия node
             continue
-
         new_distance = distance[source] + weight
-
         if new_distance < distance[destination]:
             distance[destination] = new_distance
-            parent[destination] = source
-            updated = True
+            parent[destination] = source       # за да стигнем до дестинацията, трябва да минем през източника
+####################### Bellman-Ford ###########################
 
-    if not updated:
-        break
-
-for source, destination, weight in graph:
+for source, destination, weight in graph:     # втори loop при Bellman-Ford, ако се намери нова по-добра дистанция, влизаме в негативен цикъл
     new_distance = distance[source] + weight
     if new_distance < distance[destination]:
         print('Undefined')
         break
 else:
-    path = reconstruct_path(parent, target)
-
+    path = find_path(parent, target)
     print(*path, sep=' ')
     print(distance[target])
