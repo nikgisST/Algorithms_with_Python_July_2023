c637cfe32db08f987ddca362cdfd611b4f2aca32
Nikoleta <149585913+nikgisST@users.noreply.github.com>
2024-08-30 15:04:31 +0300

    Update 3.arbitrage.py

--- 12 - Exam Preparation/3.arbitrage.py
+++ 12 - Exam Preparation/3.arbitrage.py
@@ -18,21 +18,21 @@ for _ in range(edges):
     graph.append((source, destination, weight))
     nodes.add(source)
     nodes.add(destination)
-    
+
 start_node = input()
 distance = {node: float('-inf') for node in nodes}
 distance[start_node] = 1
 parent = {node: None for node in nodes}
 
 ############################### BELLMAN-FORD ####################################
-for _ in range(len(nodes) - 1):      # (v - 1) times
+for _ in range(len(nodes) - 1):           # (v - 1) times
     for (source, destination, weight) in graph:
         new_distance = distance[source] * weight
         if new_distance > distance[destination]:
             distance[destination] = new_distance
-            parent[destination] = source
+            parent[destination] = source    # за да стигнем до дестинацията, трябва да минем през източника
 
-for (source, destination, weight) in graph:
+for (source, destination, weight) in graph:    # втори loop при Bellman-Ford, ако се намери нова по-добра дистанция, влизаме в негативен цикъл
     new_distance = distance[source] * weight
     if new_distance > distance[destination]:
         print(True)
